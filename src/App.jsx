ce5c52fcfd5d5a006d5abbcda66bcedbf8c8750f
tarantino23 <tarassukho@gmail.com>
2021-12-27 16:38:37 -0500

    set up starting front-end files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import LoginForm from './Components/LoginForm';
+import LandingPage from './Containers/LandingPage';
+
+// RENDERS landing page
+function App() {
+  return (
+    <div>
+      {/* Hello this is the App */}
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from '../Components/LoginForm';
+import SignUpForm from '../Components/SignUpForm';
+
+function LandingPage() {
+  return (
+    <div>
+      <LoginForm/>
+      <SignUpForm/>
+    </div>
+  );
+}
+
+export default LandingPage;
