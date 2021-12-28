ff1524946bf4f0fb502b9d89e4edee35f2da4feb
Anna Falvello <anna.falvello@gmail.com>
2021-12-28 11:24:24 -0500

    Database.sql file updated to create SUBSCRIBERS table

--- database/database.sql
+++ database/database.sql
@@ -13,6 +13,11 @@ CREATE TABLE USERS (
   "name_last" varchar NOT NULL
 );
 
+CREATE TABLE SUBSCRIBERS (
+  "_id" serial PRIMARY KEY,
+  "user_id" serial references USERS (_id),
+  "phone" varchar NOT NULL
+);
 
 
 
@@ -22,10 +27,18 @@ CREATE TABLE USERS (
 --psql -d <url from elephantSQL> -f database.sql
 --psql -d postgres://uriopwhg:OCX1frU-A_ry4hyeec1kgxLnOpJAcKnj@castor.db.elephantsql.com/uriopwhg -f database/database.sql
 
+/* 
+***** EXAMPLE QUERIES ***** 
 
---USEFUL QUERIES. DELETE BEFORE MERGE
+--USERS--
 
---SELECT * FROM USERS
+SELECT * FROM USERS
+DELETE FROM USERS *
+INSERT INTO USERS (username, password, name_first, name_last)
+VALUES ('dragonite', 'password', 'Dragonite', 'Doughnut');
 
---INSERT INTO USERS (username, password, name_first, name_last)
---VALUES ('testUser', 'hty', 'First', 'Last');
+--SUBSCRIBERS--
+INSERT INTO SUBSCRIBERS (user_id, phone)
+VALUES ('2', '8881450894');
+
+*/
