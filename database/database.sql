b893a22b8dce4335171a47104b8a87a28e152c0d
Anna Falvello <anna.falvello@gmail.com>
2021-12-28 10:58:55 -0500

    postgres database link added. query to create USERS table added

--- database/database.sql
+++ database/database.sql
@@ -1,12 +1,31 @@
 -- Put all of our SQL queries here 
--- URL:
--- Host name/Address: 
+-- URL: postgres://uriopwhg:OCX1frU-A_ry4hyeec1kgxLnOpJAcKnj@castor.db.elephantsql.com/uriopwhg
+-- Host name/Address: castor.db.elephantsql.com (castor-01)
 -- Maintenance database: 
--- Username: 
--- PW: 
+-- Username: uriopwhg
+-- PW: OCX1frU-A_ry4hyeec1kgxLnOpJAcKnj
+
+CREATE TABLE USERS (
+  "_id" serial PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "name_first" varchar NOT NULL,
+  "name_last" varchar NOT NULL
+);
+
+
 
 
 
 
 --Command to run our database.sql file
 --psql -d <url from elephantSQL> -f database.sql
+--psql -d postgres://uriopwhg:OCX1frU-A_ry4hyeec1kgxLnOpJAcKnj@castor.db.elephantsql.com/uriopwhg -f database/database.sql
+
+
+--USEFUL QUERIES. DELETE BEFORE MERGE
+
+--SELECT * FROM USERS
+
+--INSERT INTO USERS (username, password, name_first, name_last)
+--VALUES ('testUser', 'hty', 'First', 'Last');
