62a03364973694fbf42404191d0112c7e66841ae
connor-gillis <connoregillis@gmail.com>
2021-12-29 11:17:31 -0500

    DB URL fix

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv'); // Allows us to safely store and reference credentials in a .env file that is not uploaded to github
 dotenv.config({ path: '../.env' }); 
 
-const PG_URI = process.env.PG_URI;
+const PG_URI = PG_URI='postgres://uriopwhg:OCX1frU-A_ry4hyeec1kgxLnOpJAcKnj@castor.db.elephantsql.com/uriopwhg';
 
 // create a new pool here using the connection string above
 const pool = new Pool({
